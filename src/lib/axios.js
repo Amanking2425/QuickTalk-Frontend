027d530f106850594db01bf4b5dbc79fc71c04d1
Aman kumar <117091469+Amanking2425@users.noreply.github.com>
2025-11-29 19:24:57 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -9,18 +9,42 @@
 //     withCredentials: true ,
 // })
 
+
+
+
+// import axios from "axios";
+
+// const BASE_URL =
+//   import.meta.env.MODE === "development"
+//     ? "https://quick-talk-backend-six.vercel.app/api"
+//     : "/api";
+
+// export const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// // ✅ AUTO ATTACH TOKEN TO EVERY REQUEST
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "https://quick-talk-backend-six.vercel.app/api"
-    : "/api";
+// ✅ ALWAYS USE BACKEND URL (for both dev & prod)
+const BASE_URL = "https://quick-talk-backend-six.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-// ✅ AUTO ATTACH TOKEN TO EVERY REQUEST
+// ✅ ATTACH TOKEN TO EVERY REQUEST
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
